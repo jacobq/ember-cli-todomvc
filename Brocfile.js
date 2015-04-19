964cff567c6b96c367397a627bf8a20a831a28b8
Bill Broughton <wlbrough@gmail.com>
2015-04-19 06:42:49 -0400

    Added todomvc base and created first route

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,7 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('node_modules/todomvc-common/base.css');
+app.import('node_modules/todomvc-app-css/index.css');
+
 module.exports = app.toTree();
