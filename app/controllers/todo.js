2da44991a768eb0476b5994e394209299afee6cb
Bill Broughton <wlbrough@gmail.com>
2015-04-22 10:24:04 -0400

    Fixed deprecated ObjectController

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -1,27 +1,24 @@
 import Ember from 'ember';
 
-export default Ember.ObjectController.extend({
+export default Ember.Controller.extend({
   actions: {
     editTodo: function() {
-      this.set('isEditing', true);
+      this.model.set('isEditing', true);
     },
     acceptChanges: function() {
-      this.set('isEditing', false);
+      this.model.set('isEditing', false);
 
-      if (Ember.isEmpty(this.get('model.title'))) {
+      if (Ember.isEmpty(this.model.get('title'))) {
         this.send('removeTodo')
       } else {
         this.get('model').save();
       }
     },
     removeTodo: function() {
-      this.removeTodo();
+      var todo = this.get('model');
+      todo.deleteRecord();
+      todo.save();
     }
-  },
-  removeTodo: function () {
-    var todo = this.get('model');
-    todo.deleteRecord();
-    todo.save();
   },
   isEditing: false,
   isCompleted: function(key, value) {
