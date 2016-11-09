d78f675de6189c02e6724a86f0f3204a66a630eb
Jacob Quant <jacob.quant@kanomaxfmt.com>
2016-11-09 10:46:36 -0600

    WIP: Reformatted code

--- app/components/edit-todo.js
+++ app/components/edit-todo.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.TextField.extend({
-  didInsertElement: function() {
-    this.$().focus();
-  }
+    didInsertElement: function () {
+        this.$().focus();
+    }
 });
--- app/models/todo.js
+++ app/models/todo.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  title: DS.attr('string'),
-  isCompleted: DS.attr('boolean')
+    title: DS.attr('string'),
+    isCompleted: DS.attr('boolean')
 });
